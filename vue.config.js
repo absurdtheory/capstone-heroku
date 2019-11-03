87f27cb722f9b8335d44317871589dadd9959971
absurdtheory <38075151+absurdtheory@users.noreply.github.com>
2019-11-03 08:05:32 -0500

    updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
     // On Linux you might want to set publicPath: "http://0.0.0.0:8080/"
-    publicPath: "http://127.0.0.1:8080/",
+    publicPath: "https://hello-grandpa.herokuapp.com/",
     outputDir: './dist/',
     transpileDependencies: ["vuetify"],
 
@@ -23,9 +23,9 @@ module.exports = {
 
         config.devServer
             // the first 3 lines of the following code have been added to the configuration
-            .public('http://127.0.0.1:8080')
-            .host('127.0.0.1')
-            .port(8080)
+            //.public('http://127.0.0.1:8080')
+            //.host('127.0.0.1')
+            //.port(8080)
             .hotOnly(true)
             .watchOptions({poll: 1000})
             .https(false)
