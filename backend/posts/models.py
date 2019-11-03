4cc73e2ab413b6b60e08ca072174347df5855895
absurdtheory <38075151+absurdtheory@users.noreply.github.com>
2019-11-03 07:24:51 -0500

    updated

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.CharField(max_length=240)
-    url = models.CharField(max_length=240)
+    #url = models.CharField(max_length=240)
     slug = models.SlugField(max_length=255, unique=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL,
                                on_delete=models.CASCADE,
